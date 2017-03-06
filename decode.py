248a483206e4a36022c5fde86f63f85b41df8943
Josh Shi <thehandsomepanther@gmail.com>
2017-03-06 17:08:41 -0600

    actual steganography

--- decode.py
+++ decode.py
@@ -1,4 +1,6 @@
 import librosa
+from scipy.io.wavfile import write
+from librosa.core import istft
 from librosa.core import stft
 from librosa.core import load
 import numpy as np
@@ -8,10 +10,23 @@ RATE = 44100
 WINDOW_LENGTH = 2048
 HOP_SIZE = 1024
 
-def decode(wavfile):
+def wavwrite(filepath, data, sr, norm=True, dtype='int16'):
+    if norm:
+        data /= np.max(np.abs(data))
+    data = data * np.iinfo(dtype).max
+    data = data.astype(dtype)
+    write(filepath, sr, data)
+
+def decode(wavfile, key_file):
     signal, sr = librosa.load(wavfile, sr=RATE)
     spec = stft(signal, WINDOW_LENGTH, HOP_SIZE)
     message = ""
+
+    if key_file is not None:
+        key_signal, sr = librosa.load(key_file, sr=RATE)
+        # signal = np.pad(signal, (0, key_signal.shape[0] - signal.shape[0]), 'edge')
+        spec = np.subtract(stft(key_signal, WINDOW_LENGTH, HOP_SIZE), spec)
+        wavwrite('minus.wav', istft(spec, 1024, 2048), RATE)
 
     for i in range(1, spec.shape[1]):
         h = np.argmax(np.abs([spec[x][i] for x in range(spec.shape[0])]))
@@ -22,10 +37,11 @@ def decode(wavfile):
 
         char = str(chr(h))
         message += char
+
     return message
 
 def main():
-    print decode(sys.argv[1])
+    print decode(sys.argv[1], sys.argv[2])
 
 if __name__ == "__main__":
     main()
--- encode.py
+++ encode.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.io.wavfile import write
+import librosa
 from librosa.core import istft
 from librosa.core import stft
 import os
@@ -20,28 +21,46 @@ def make_sinewave(f, t, sr):
     vals = np.array([2 * np.pi * x * f / sr for x in range(int(sr * t))])
     return np.sin(vals)
 
-def encode(data_file, output_file):
+def encode(data_file, output_file, key_file=None):
     reps = 1
     data_file_size = os.path.getsize(data_file)
-    signal = make_sinewave(900, data_file_size*reps/25, RATE)
-    spec = stft(signal, WINDOW_LENGTH, HOP_SIZE)
 
-    with open(data_file) as dfile:
-        d = dfile.read(1)
-        i = 0
-        while d:
-            h = int(d.encode("hex"), 16)
-            for r in range(reps):
-                spec[h-1][i+r] = 0
-                spec[h][i+r] = np.max(np.abs([spec[x][i+r] for x in range(spec.shape[0])])) * 200
-                spec[h+1][i+r] = 0
+    if key_file is None:
+        signal = make_sinewave(900, data_file_size*reps/25, RATE)
+        spec = stft(signal, WINDOW_LENGTH, HOP_SIZE)
+
+        with open(data_file) as dfile:
             d = dfile.read(1)
-            i += reps
+            i = 0
+            while d:
+                h = int(d.encode("hex"), 16)
+                for r in range(reps):
+                    spec[h-1][i+r] = 0
+                    spec[h][i+r] = np.max(np.abs([spec[x][i+r] for x in range(spec.shape[0])])) * 200
+                    spec[h+1][i+r] = 0
+                d = dfile.read(1)
+                i += reps
+    else:
+        signal, sr = librosa.load(key_file, sr=RATE)
+        spec = stft(signal, WINDOW_LENGTH, HOP_SIZE)
+
+        with open(data_file) as dfile:
+            d = dfile.read(1)
+            i = 0
+            while d:
+                h = int(d.encode("hex"), 16)
+                for r in range(reps):
+                    spec[h-1][i+r] = 0
+                    spec[h][i+r] = np.max(np.abs([spec[x][i+r] for x in range(spec.shape[0])])) + 200
+                    spec[h+1][i+r] = 0
+                d = dfile.read(1)
+                i += reps
 
     wavwrite(output_file, istft(spec, 1024, 2048), RATE)
 
+
 def main():
-    encode(sys.argv[1], sys.argv[2])
+    encode(sys.argv[1], sys.argv[2], sys.argv[3])
 
 if __name__ == "__main__":
     main()
